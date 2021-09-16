8eff4f945b15c867cfa0c7e77b9c59974a455809
Do_Lam_Truong <truong.do2304@hcmut.edu.vn>
2021-09-16 11:07:42 +0700

    db for taxi

--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
-CREATE TABLE driverlocation1 (
-	id serial PRIMARY KEY,
-	latitude double precision NOT NULL,
-	longitude double precision NOT NULL,
-	h3index bigint NOT NULL,
-	driverid int NOT NULL,
-    active bool NOT NULL
-);
-CREATE index on driverlocation2(h3index);
-CREATE index on driverlocation2(driverid);
+-- CREATE TABLE driverlocation1 (
+-- 	id serial PRIMARY KEY,
+-- 	latitude double precision NOT NULL,
+-- 	longitude double precision NOT NULL,
+-- 	h3index bigint NOT NULL,
+-- 	driverid int NOT NULL,
+--     active bool NOT NULL
+-- );
+CREATE index on driverlocation1(h3index);
+CREATE index on driverlocation1(driverid);
 
